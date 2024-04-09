30fe9c5688564202a39efd0ff4a8a333b27eef6a
Gabriel Peres <55238431+GabePeres@users.noreply.github.com>
2024-04-09 10:09:41 -0300

    Add files via upload

---
+++ aulaDio.rb
@@ -0,0 +1,12 @@
+#comentário
+
+print 'digite seu nome: '
+nome = gets.chomp
+
+print 'digite seu sobrenome: '
+sobrenome = gets.chomp
+
+print 'digite sua idade: '
+idade = gets.chomp.to_i
+
+puts "Cadastro: \n nome: #{nome} #{sobrenome} \nidade: #{idade}"
